28894640de2ea049f10a62ca949876906f0d45cc
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-22 17:35:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ function updateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign({}, driver, { [key]: value });
 };
 
-function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
-  obj[key] = value;
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+
+  return driver;
 }
 
 const driver2 = {};
